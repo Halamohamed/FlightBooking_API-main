e8a58685b84bf3196187a6156533e228a4354a19
Halamohamed <halaalimohammed@gmail.com>
2026-06-02 13:10:50 +0200

    added vercel config

--- src/main/java/se/lexicon/flightbooking_api/config/CorsConfig.java
+++ src/main/java/se/lexicon/flightbooking_api/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableConfigurationProperties(CorsProperties.class)
@@ -28,5 +30,18 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173",
+                                "https://FlightBooking_API.vercel.app")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
